7b954d6905f4df12525e5730e26b04ba0e7be802
Dominic Tham <dtham@signnow.com>
2014-05-30 13:31:00 -0700

    Check for out of range

--- AddToYourProject/LTHPasscodeViewController.m
+++ AddToYourProject/LTHPasscodeViewController.m
@@ -656,6 +656,10 @@ static CGFloat const kSlideAnimationDuration = 0.15f;
 
 - (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string {
 	if ([string isEqualToString: @"\n"]) return NO;
+    
+    if(range.length > [textField.text length]){
+        return NO;
+    }
 	
 	NSString *typedString = [textField.text stringByReplacingCharactersInRange: range
 																	withString: string];
